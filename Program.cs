fe519a831b9229d9ae1e56b0356848c70c9e3c36
igorcomputer <igorcomputer@mail.ru>
2023-04-15 01:14:23 +0300

    Add Responce to Users of bot for command /start

--- Program.cs
+++ Program.cs
@@ -12,12 +12,22 @@ namespace LangTranslationTelegramBot
 
         static TelegramBotClient Bot;
 
+        const string COMMAND_LIST =
+@"
+/add <eng> <rus> - Добавление английского слова и его перевод в словарь
+/get - получаем случайное английское слово из словаря
+/check <eng> <rus> - проверяем правильность перевода английского слова
+";
+
         [Obsolete]
         static void Main(string[] args)
         {
 
             Bot = new TelegramBotClient("6191785024:AAEYEPcVg7oyd1C5xweaJc3kRFJLYJUQew4");
 
+
+
+            // TEST 
             //var me = Bot.GetMeAsync().Result;
             //Console.WriteLine(me.FirstName);
 
@@ -30,6 +40,18 @@ namespace LangTranslationTelegramBot
 
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
+            if (e == null || e.Message == null || e.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return;
+
+            var msgArgs = e.Message.Text.Split(' ');
+
+            switch (msgArgs[0])
+            {
+                case "/start":
+                    Bot.SendTextMessageAsync(e.Message.From.Id, COMMAND_LIST);
+                    break;
+            }
+
             Console.WriteLine(e.Message.Text);
             Console.WriteLine(e.Message.From.Username);
         }
